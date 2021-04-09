82cf54bb195fd8697893960a3ee4878855cb4e34
fareedh <mdfareedh01@gmail.com>
2021-04-09 21:50:43 +0530

    "second commit after mysql"

--- server/src/wege/settings.py
+++ server/src/wege/settings.py
@@ -38,6 +38,7 @@ CORS_ORIGIN_WHITELIST = (
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
