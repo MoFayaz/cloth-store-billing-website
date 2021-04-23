bd0b9a3053806b517b38f9c53fba6971d44f4ebc
Mohamed Fayaz <55308975+MoFayaz@users.noreply.github.com>
2021-04-23 19:15:35 +0530

    Delete server/src directory

--- server/src/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProductCodeView, ProductView
-
-
-urlpatterns = [
-    path('products/<str:pcode>/', ProductCodeView.as_view(), name='product-code'),
-    path('products/', ProductView.as_view(), name='product' )
-]
